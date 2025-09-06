a97403cb34a13f0960d151af6ce2e0d33e787549
Rashmi <rashmim2405@gmail.com>
2025-09-06 13:26:53 +0530

    Deploying again

--- src/main/java/com/example/portfolio/controller/HomeController.java
+++ src/main/java/com/example/portfolio/controller/HomeController.java
@@ -7,7 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class HomeController {
 
     @GetMapping({"","/","/home"})
-    public String showHomePage(){
+    public  String showHomePage(){
         return "home";
     }
 }
+/* controller file*/
